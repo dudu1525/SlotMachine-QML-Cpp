05b7035dbf77df5badec64cd1aaa67bd91901789
Dan <serbandan2004@gmail.com>
2025-02-09 21:16:27 +0200

    Updated .gitignore to exclude debug folder

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appslot_qt_FILE D:/proiecte/qt/slot_qt/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/appslot_qt.exe)
-set(__QT_DEPLOY_TARGET_appslot_qt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appslot_qt_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
